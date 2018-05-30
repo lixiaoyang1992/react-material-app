8f8027b5a748ad8f837d86cb5796688f646b4938
lixiaoyang1992 <lixiaoyang2345@gmail.com>
2018-05-30 08:06:54 +0800

    add loadable

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,8 +3,21 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
-import Main from "./Main";
-import Login from "./Login";
+import * as Loadable from "react-loadable";
+
+const Loading = () => {
+  return <div>Loading...</div>;
+};
+
+const LoadableMian = Loadable({
+  loader: () => import("./Main"),
+  loading: Loading
+});
+
+const LoadableLogin = Loadable({
+  loader: () => import("./Login"),
+  loading: Loading
+});
 
 const theme = createMuiTheme({
   palette: {
@@ -17,8 +30,8 @@ const App = () => (
     <Router>
       <>
         <CssBaseline />
-        <Route exact={true} path="/" component={Main} />
-        <Route path="/login" component={Login} />
+        <Route exact={true} path="/" component={LoadableMian} />
+        <Route path="/login" component={LoadableLogin} />
       </>
     </Router>
   </MuiThemeProvider>
