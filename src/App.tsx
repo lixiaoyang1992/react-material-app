c9cf45bf83fcb4d9c8cf98c6d01286e5fdc81bca
lixiaoyang1992 <lixiaoyang2345@gmail.com>
2018-07-21 16:18:57 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import * as React from "react";
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import sagaMonitor from "./sagaMonitor";
-import { Provider } from "react-redux";
+import * as React from 'react';
 
-import Routers from "./Router";
-import reducers from "./reducer";
+import Routers from './Router';
 
-// const middleware = [];
-if (process.env.NODE_ENV !== "production") {
-  // middleware.push(createLogger());
-}
-
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
-
-const App = () => (
-  <Provider store={store}>
-    <Routers />
-  </Provider>
-);
+const App = () => <Routers />;
 
 export default App;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,28 +1,28 @@
-import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import blue from "@material-ui/core/colors/blue";
-import * as Loadable from "react-loadable";
+import * as React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import * as Loadable from 'react-loadable';
 
 const Loading = () => {
   return <div>Loading...</div>;
 };
 
 const LoadableMian = Loadable({
-  loader: () => import("./Main"),
-  loading: Loading
+  loader: () => import('./Main'),
+  loading: Loading,
 });
 
 const LoadableLogin = Loadable({
-  loader: () => import("./Login"),
-  loading: Loading
+  loader: () => import('./Login'),
+  loading: Loading,
 });
 
 const theme = createMuiTheme({
   palette: {
-    primary: blue
-  }
+    primary: blue,
+  },
 });
 
 const App = () => (
