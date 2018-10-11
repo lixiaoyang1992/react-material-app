bccdda79bd6d78cd1e1f9f329cbccb41006f1f28
lixiaoyang1992 <lixiaoyang2345@gmail.com>
2018-10-11 09:50:36 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,53 @@
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import * as en from 'react-intl/locale-data/en';
+import * as zh from 'react-intl/locale-data/zh';
+import zhCN from './locales/zh-CN';
+import enUS from './locales/en-US';
 import Routers from './Router';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue
+  }
+});
+
+addLocaleData(en);
+addLocaleData(zh);
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql'
 });
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <Routers />
-  </ApolloProvider>
-);
+const App = () => {
+  // 国际化
+  const language = localStorage.getItem('language') || 'zh';
+  let locale;
+  let messages;
+  if (language === 'zh') {
+    locale = 'zh';
+    messages = zhCN;
+  } else if (language === 'en') {
+    locale = 'en';
+    messages = enUS;
+  }
+  return (
+    <ApolloProvider client={client}>
+      <IntlProvider locale={locale} messages={messages}>
+        <MuiThemeProvider theme={theme}>
+          <>
+            <CssBaseline />
+            <Routers />
+          </>
+        </MuiThemeProvider>
+      </IntlProvider>
+    </ApolloProvider>
+  );
+};
 
 export default App;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,47 +1,12 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
-import { IntlProvider, addLocaleData } from 'react-intl';
-import * as en from 'react-intl/locale-data/en';
-import * as zh from 'react-intl/locale-data/zh';
-import zhCN from './locales/zh-CN';
-import enUS from './locales/en-US';
 import Layout from './Layout';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: blue
-  }
-});
-
-addLocaleData(en);
-addLocaleData(zh);
-
 const App = () => {
-  // 国际化
-  const language = localStorage.getItem('language') || 'zh';
-  let locale;
-  let messages;
-  if (language === 'zh') {
-    locale = 'zh';
-    messages = zhCN;
-  } else if (language === 'en') {
-    locale = 'en';
-    messages = enUS;
-  }
   return (
-    <IntlProvider locale={locale} messages={messages}>
-      <MuiThemeProvider theme={theme}>
-        <Router>
-          <>
-            <CssBaseline />
-            <Route path="/" render={props => <Layout {...props} />} />
-          </>
-        </Router>
-      </MuiThemeProvider>
-    </IntlProvider>
+    <Router>
+      <Route path="/" render={props => <Layout {...props} />} />
+    </Router>
   );
 };
 
